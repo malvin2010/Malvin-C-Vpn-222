6fa255f4a4a312846da49534a55868d498f2e6df
malvin2010 <malvinchindondondo8@gmail.com>
2026-06-04 22:45:52 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "malvin-c-vpn",
+  "version": "1.0.0",
+  "description": "Malvin C Vpn WhatsApp Bot - Powered by Handsome Tech Zimbabwe",
+  "main": "src/index.js",
+  "scripts": {
+    "start": "node src/index.js",
+    "web": "node src/web.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.9",
+    "express": "^4.18.2",
+    "qrcode": "^1.5.3",
+    "pino": "^8.15.0",
+    "axios": "^1.6.0",
+    "cheerio": "^1.0.0-rc.12",
+    "fs-extra": "^11.1.1",
+    "node-cache": "^5.1.2",
+    "ytdl-core": "^4.11.5",
+    "fluent-ffmpeg": "^2.1.2",
+    "cors": "^2.8.5"
+  }
+      }
